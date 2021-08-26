2a4e83ac300009c203233dcc278fc568f0320e3e
samwato <sambenwatson@gmail.com>
2021-08-26 18:20:18 +1000

    fix

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,7 +3,7 @@ console.log('Hello World')
 window.TrelloPowerUp.initialize({
   'card-badges': function(t, opts) {
     // return an array of card badges for the given card
-    return t.card('call')
+    return t.card('all')
     .then(function(card) {
       console.log(card)
       return [
